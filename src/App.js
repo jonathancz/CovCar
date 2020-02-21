b4a59187e37d875fa3a9597c7fe4012c10267e2c
Julian Laury <julian.laury@hotmail.com>
2020-02-21 15:05:36 -0500

    Added a two basic links which go to either the driver or rider page. Any code for the driver should go under the Driver component and any code for the rider should go under the Rider component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>CovCar</h1>
+      <header className="appHeader">
+        <h1>Welcome to CovCar!</h1>
       </header>
+      <body>
+        <div className="userButtons"><InputForm /></div>
+      </body>
     </div>
   );
+}
+
+class InputForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleDriverButtonClick() {
+
+  }
+
+  handleRiderButtonClick() {
+
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="buttons">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/driver">I am a driver</Link>
+              </li>
+              <li>
+                <Link to="/rider">I am a rider</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <hr />
+
+          <Route exact path="/" component={App} />
+          <Route path="/driver" component={Driver} />
+          <Route path="/rider" component={Rider} />
+        </div>
+      </Router>
+    )}
+  }
+
+         
+class Driver extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="DriverPage">
+        <h1>You are on the driver page.</h1>
+      </div>
+    )
+  }
+}
+
+class Rider extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="RiderPage">
+        <h1>You are on the rider page.</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
