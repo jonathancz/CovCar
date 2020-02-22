968e9a61898eeca0b3d03cb507b50f628184bdd7
Julian Laury <julian.laury@hotmail.com>
2020-02-21 21:20:15 -0500

    Added fields for new driver

--- src/App.js
+++ src/App.js
@@ -51,59 +51,142 @@ class InputForm extends React.Component {
           <hr />
           <Route path="/driver" component={Driver} />
           <Route path="/rider" component={Rider} />
+          <Route path="/newdriver" component={NewDriver}/>
         </div>
       </Router>
     )}
   }
 
-const pickupOptions = [
-    'Carter Circle', 'Mac Circle'
-  ] 
 class Driver extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      newDriver: ''
+    }
   }
 
- 
+  render() {
+    return(
+      <div className="driverOptions">
+        <Link to="/newdriver">I am a new driver.</Link><br></br>
+        <Link to="/returningdriver">I am a returning driver.</Link>
+      </div>
+    )
+  }
+}
 
-  addDriver() {
-      var driver = {
-        DriverId: null,
-        FirstName: null
-      };
+class NewDriver extends React.Component {
+  constructor(props) {
+    super(props);
 
-      
+    this.state = {
+      firstName: '',
+      lastName: '',
+      churchName: '',
+      denomination: '',
+      departureDate: null,
+      departureTime: '',
+      pickupPoint: 'carterCircle',
+      numberOfSeats: 0,
+      phone: '',
+      email: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDropDown = this.handleDropDown.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+      //alert('First Name: ' + this.state.firstName);   
+  }
+
+  handleSubmit(event) {
+    alert('Your entry: ' + JSON.stringify(this.state));   
+  }
+
+  handleDropDown(selection) {
+    this.setState({
+      pickupPoint: selection
+    })
   }
 
   render() {
     return (
-      <div className="DriverPage">
-        <h1>You are on the driver page.</h1>
+      <form className="DriverPage">
+        <h2>You are on the new driver page.</h2>
         <div className="entryFields">
           <label>
             First Name: 
-            <input type="text" name="firstName" />
+            <input type="text" name="firstName" onChange={this.handleChange}/>
           </label><br></br>
           <label>
             Last Name: 
-            <input type="text" name="lastName" />
+            <input type="text" name="lastName" onChange={this.handleChange} />
           </label><br></br>
           <label>
             Church Name: 
-            <input type="text" name="churchName" />
+            <input type="text" name="churchName" onChange={this.handleChange} />
           </label><br></br>
           <label>
             Denomination: 
-            <input type="text" name="denomination" />
+            <input type="text" name="denomination" onChange={this.handleChange} />
+          </label><br></br>
+          <label>
+            Date: 
+            <input type="date" name="departureDate" onChange={this.handleChange} />
           </label><br></br>
           <label>
             Departure Time: 
-            <input type="text" name="departureTime" />
+            <input type="text" name="departureTime" onChange={this.handleChange} />
           </label><br></br>
-          <input type="submit" value="Submit" />
+          <select name="pickupPoint" onChange={this.handleChange}>
+            <option value="carterCircle">Carter Circle</option>
+            <option value="macCircle">Mac Circle</option>
+          </select><br></br>
+          <label>
+            Number Of Seats: 
+            <input type="number" name="numberOfSeats" onChange={this.handleChange} />
+          </label><br></br>
+          <label>
+            Phone Number: 
+            <input type="tel" name="phone" onChange={this.handleChange} />
+          </label><br></br>
+          <label>
+            Email: 
+            <input type="text" name="email" onChange={this.handleChange} />
+          </label><br></br>
         </div>
-      </div>
+        <button type="submit" onClick={this.handleSubmit}>Submit</button>
+      </form>
+    )
+  }
+}
+
+class PickupDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    /*console.log(event.target.value);
+    this.state = {value: event.target.value};*/
+    var selection = event.target.value;
+    this.props.onPickupDropdown(selection);
+  }
+
+  render () {
+    return (
+      <select className="pickupDropdown" value={this.state.value} onChange={this.handleChange}>
+        <option value="carterCircle">Carter Circle</option>
+        <option value="macCircle">Mac Circle</option>
+      </select>
     )
   }
 }
