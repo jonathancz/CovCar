d88c4fc194aa0d465bf26f04d6d457df78976e5c
Julian Laury <julian.laury@hotmail.com>
2020-02-21 23:17:45 -0500

    Working on database integation, there's an issue with doing it from the javascript

--- src/App.js
+++ src/App.js
@@ -75,6 +75,7 @@ class Driver extends React.Component {
   }
 }
 
+// Component for creating a new driver entry
 class NewDriver extends React.Component {
   constructor(props) {
     super(props);
@@ -84,8 +85,8 @@ class NewDriver extends React.Component {
       lastName: '',
       churchName: '',
       denomination: '',
-      departureDate: null,
       departureTime: '',
+      departureDate: null,
       pickupPoint: 'carterCircle',
       numberOfSeats: 0,
       phone: '',
@@ -93,10 +94,10 @@ class NewDriver extends React.Component {
     }
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleDropDown = this.handleDropDown.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  // Inserts the inputted data into the state object
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value
@@ -104,19 +105,42 @@ class NewDriver extends React.Component {
       //alert('First Name: ' + this.state.firstName);   
   }
 
+  // Handler for submit button to create a new driver
   handleSubmit(event) {
-    alert('Your entry: ' + JSON.stringify(this.state));   
+    //alert('Your entry: ' + JSON.stringify(this.state)); 
+    
+    // Add new driver data to database
+
+    // QUERY - hardcoded for now, idk if there's a better way to do it
+    var sql = 'INSERT INTO DRIVERS(FirstName, LastName, Church, Denomination, DepartureTime, DepartureDate, PickupPoint, NumberOfSeats, PhoneNumber, Email) VALUES (' +
+      this.state.firstName + ', ' + this.state.lastName + ', ' + this.state.churchName + ', ' + this.state.denomination + ', ' + this.state.departureTime + ', ' +
+      this.state.departureDate + ', ' + this.state.pickupPoint + ', ' + this.numberOfSeats + ', ' + this.state.phone + ', ' + this.state.email + ');';
+
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+      host: 'localhost',
+      username: 'covcar',
+      password: 'covcar2020sql',
+      database: 'covcar'
+    });
+    con.connect((err) => {
+      if (err) throw err;
+      console.log("Connected!");
+      con.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("Result: " + result);
+        return (
+          <NewDriverSuccess name={this.state.firstName + this.state.lastName}/>
+        )
+    });
+  });
   }
 
-  handleDropDown(selection) {
-    this.setState({
-      pickupPoint: selection
-    })
-  }
-
+  // Form for the driver input fields
   render() {
     return (
-      <form className="DriverPage">
+      <div className="DriverPage">
         <h2>You are on the new driver page.</h2>
         <div className="entryFields">
           <label>
@@ -161,35 +185,50 @@ class NewDriver extends React.Component {
           </label><br></br>
         </div>
         <button type="submit" onClick={this.handleSubmit}>Submit</button>
-      </form>
+      </div>
     )
   }
 }
 
-class PickupDropdown extends React.Component {
+class NewDriverSuccess extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
   }
-
-  handleChange = (event) => {
-    /*console.log(event.target.value);
-    this.state = {value: event.target.value};*/
-    var selection = event.target.value;
-    this.props.onPickupDropdown(selection);
-  }
-
-  render () {
+  render() {
     return (
-      <select className="pickupDropdown" value={this.state.value} onChange={this.handleChange}>
-        <option value="carterCircle">Carter Circle</option>
-        <option value="macCircle">Mac Circle</option>
-      </select>
+      <div className="newDriverSuccess">
+        <h2>Thank you, {this.props.name}, you were successfully added to the database!</h2>
+      </div>
     )
   }
 }
+
+// TODO: remove this later, I'm pretty sure it's useless
+// class PickupDropdown extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: ''};
+
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+
+//   handleChange = (event) => {
+//     /*console.log(event.target.value);
+//     this.state = {value: event.target.value};*/
+//     var selection = event.target.value;
+//     this.props.onPickupDropdown(selection);
+//   }
+
+//   render () {
+//     return (
+//       <select className="pickupDropdown" value={this.state.value} onChange={this.handleChange}>
+//         <option value="carterCircle">Carter Circle</option>
+//         <option value="macCircle">Mac Circle</option>
+//       </select>
+//     )
+//   }
+// }
 
 class Rider extends React.Component {
   constructor(props) {
